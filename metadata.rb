a3d325f371ca721430bc71eea045383a21005e33
Aaron Walker <a.walker@base2services.com>
2017-01-18 14:09:19 +1000

    updates cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,8 @@ maintainer_email 'a.walker@base2services.com'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures AWS Cloudwatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/base2Services/cloudwatch_logs_cookbook'
+issues_url      'https://github.com/base2Services/cloudwatch_logs_cookbook/issues'
 version          '0.1.0'
+
+supports 'amazon'
